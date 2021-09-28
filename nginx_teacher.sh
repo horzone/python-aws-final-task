651c8dec48bef13e108959df893df30e59598de4
hrz <bagzina@gmail.com>
2021-09-28 09:16:27 +0300

    fix

--- nginx_teacher.sh
+++ nginx_teacher.sh
@@ -9,5 +9,6 @@ echo -e "<center><h2>IP: $MY_IP</h2><br><h2>REGION: $MY_REGION</h2><br><h2>AZ: $
 sudo useradd -m -G adm -s /bin/bash teacher
 sudo mkdir /home/teacher/.ssh
 echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCkBIEsfJD6d0J4tqTnVq4z3Ve0bop71b+27j75gncRsLdAokiUqGyt1fmvS+VkoBWxOFiAOsfdSdTwJWyGs0kplZouOh93cRc/9mp16mNcR5B86+ORLrMZCq3ZGVj2F3YjtoeK4b+RvEtpRmJaC/no9yjTeDTnBYVsV+vQvxiaaeLzkbPRhd0Ovlayoz/gXqI4DOCaQTfISHxG7X+NLfpWR6LsfaHYKG/5WmaA3LOnYAqV+S7nq2WQVQ2Z5bzpJC9s= andrey@MBP-Andrey' | sudo tee /home/teacher/.ssh/authorized_keys
+sudo chown -R teacher:teacher /home/teacher/.ssh
 sudo systemctl reload nginx.service
 sudo systemctl enable nginx.service
